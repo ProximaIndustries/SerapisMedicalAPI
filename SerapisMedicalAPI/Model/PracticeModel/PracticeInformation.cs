30aa269e48eedcd7fefd9cf528d728f96180f161
Khanyisani Buthelezi <khanyisani.buthelezi03@gmail.com>
2020-02-09 14:54:04 +0200

    .

--- SerapisMedicalAPI/Model/PracticeModel/PracticeInformation.cs
+++ SerapisMedicalAPI/Model/PracticeModel/PracticeInformation.cs
@@ -10,6 +10,8 @@ namespace SerapisMedicalAPI.Model.DoctorModel.Practice
     {
         public ObjectId Id { get; set; }
 
+        public string PracticePicture { get; set; }
+
         public double Latitude { get; set; }
 
         public double Longitude { get; set; }
@@ -19,5 +21,13 @@ namespace SerapisMedicalAPI.Model.DoctorModel.Practice
         public Address PracticeAddress { get; set; }
 
         public double DistanceFromPractice { get; set; }
+
+        public List<ObjectId> DoctorsAtPractice { get; set; }
+
+        public int NumOfPatientsInPractice { get; set; }
+
+        public string OperatingTime { get; set; }
+
+        public PracticeContact ContactPractice { get; set; }
     }
 }
