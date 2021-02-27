4de732c4970f215fcf98c45307fec35713591369
Bonga Ngcobo <Bonga.Developer@outlook.com>
2021-02-27 11:44:15 +0200

    removed old stuff

--- SerapisMedicalAPI.Core/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace SerapisMedicalAPI.Core
-{
-    public class Class1
-    {
-    }
-}
--- SerapisMedicalAPI.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace SerapisMedicalAPI.Domain
-{
-    public class Class1
-    {
-    }
-}
