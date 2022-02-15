efc7cf49eb9d25e82e853fe36e565c0da61fc31c
Bonga <bonga.developer@outlook.com>
2022-02-15 02:20:15 +0200

    added cassandra db to the party

---
+++ SerapisMedicalAPI/CassandraContext.cs
@@ -0,0 +1,40 @@
+﻿using Cassandra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace SerapisMedicalAPI
+{
+    public class CassandraContext
+    {
+        private ILogger<CassandraContext> _logger;
+        private readonly ISession _session;
+        
+        public CassandraContext(ILogger<CassandraContext> logger)
+        {
+            _logger = logger;
+            
+            _logger?.LogInformation("Attempting to connect to the Cassandra Cluster");
+            var session =
+                Cluster.Builder()
+                    .WithCloudSecureConnectionBundle(@"C:\secure-connect-serapismedical.zip")
+                    //or if on linux .WithCloudSecureConnectionBundle(@"/PATH/TO/>>secure-connect-serapismedical.zip")
+                    .WithCredentials("ueIbwWaQCLecvNQwBNuvlbbE",
+                        "UbyEl3OeSrHsmQNIJQZSKmujJ_dDKcQnmz-9XSYcxOztbIZ2fcrmqQrTmrfZ3Fmwjx-BN-lSR-cpwIf6x1gX4ZJ7sZLLieqwv6rLPDE3SDs.l,_Z86gku3aOeaIHuRPW")
+                    .Build()
+                    .Connect();
+
+            _logger?.LogInformation("Server Name:  "+ session.Cluster.Metadata.ClusterName);
+            _logger?.LogInformation("Server Hosts:  "+ session.Cluster.Metadata.AllHosts());
+            _logger?.LogInformation("Server Replica's :  "+ session.Cluster.Metadata.AllReplicas());
+            _logger?.LogInformation("Session Name:  "+ session.SessionName); ;
+            
+            _session = session;
+
+        }
+
+        public ISession GetDatabaseSession => _session;
+    }
+}
---
+++ SerapisMedicalAPI/Controllers/SymptomCheckerController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SerapisMedicalAPI.Model;
+using SerapisMedicalAPI.Interfaces;
+
+namespace SerapisMedicalAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class SymptomCheckerController : Controller
+    {
+        private readonly ISymptomCheckerRepository _symptomCheckerRepository;
+        private readonly ILogger<SymptomCheckerController> _logger;
+
+        public SymptomCheckerController(ISymptomCheckerRepository symptomCheckerRepository, ILogger<SymptomCheckerController> logger)
+        {
+            _symptomCheckerRepository = symptomCheckerRepository;
+            _logger = logger;
+        }
+        
+        // GET: api/SymptomChecker
+        [HttpGet(Name = "Get")]
+        public void Get()
+        {
+            
+        }
+        
+        
+    }
+}
