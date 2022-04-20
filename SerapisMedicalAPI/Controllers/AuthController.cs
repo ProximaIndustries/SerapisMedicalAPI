4a21bda684dc79720860a728fc081691d8123c6e
bonga <bonga.developer@outlook.com>
2022-04-20 14:19:28 +0200

    pushing controller

---
+++ SerapisMedicalAPI/Controllers/AuthController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using SerapisMedicalAPI.Helpers;
+using SerapisMedicalAPI.Model;
+using SerapisMedicalAPI.Model.PatientModel;
+using SerapisMedicalAPI.Model.Supabase;
+using SerapisMedicalAPI.Services.SymptomsChecker;
+using Serilog;
+
+namespace SerapisMedicalAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/v1/[controller]")]
+    public class AuthController : Controller
+    {
+        private readonly IAccountSupabaseRepository _supabaseRepository;
+        public AuthController(IAccountSupabaseRepository supabaseRepository)
+        {
+            _supabaseRepository = supabaseRepository;
+        }
+        [HttpPost("supabase/register")]
+        [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(BaseResponse<Patient>))] 
+        [ProducesResponseType((int)HttpStatusCode.InternalServerError, Type = typeof(ErrorResponse))]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest, Type = typeof(ErrorResponse))]
+        public async Task<IActionResult>  RegisterUser([FromBody] Patient patient)
+        {
+            Log.Information($"Request:{patient}");
+            var response = await _supabaseRepository.RegisterUser(patient);
+            
+            return new OkObjectResult(response);
+        }
+        
+        [HttpPost("supabase/login")]
+        [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(Task<BaseResponse<PatientAuthResponse>>))] 
+        [ProducesResponseType((int)HttpStatusCode.InternalServerError, Type = typeof(ErrorResponse))]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest, Type = typeof(ErrorResponse))]
+        public async Task<IActionResult>  LoginUser([FromBody] SupabaseAuth patient)
+        {
+            var response = await _supabaseRepository.LoginUser(patient);
+            var encrypt = EncryptService.EncryptPlainTextToCipherText(response.ToJson().ToString());
+            var decrypt = EncryptService.DecryptCipherTextToPlainText(encrypt);
+            Log.Information(encrypt);
+            Log.Information(decrypt);
+            return new OkObjectResult(response);
+        }
+        
+        [HttpGet]
+        public IEnumerable<string> defaultroute()
+        {
+            Log.Information("Request:");
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
