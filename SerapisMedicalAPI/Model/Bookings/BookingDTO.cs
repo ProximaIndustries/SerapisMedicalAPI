1ef60433c0e292fc1c2628f4d05bc3ed71763078
bonga.ngcobo@kuda.co.za <bonga.ngcobo@kuda.co.za>
2022-05-31 14:39:04 +0200

    add Booking DTO

---
+++ SerapisMedicalAPI/Model/Bookings/BookingDTO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using SerapisMedicalAPI.Model.AppointmentModel;
+using SerapisMedicalAPI.Model.PatientModel;
+
+namespace SerapisMedicalAPI.Model.Bookings
+{
+    public class BookingDTO
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string id { get; set; }
+        
+        public string BookingId { get; set; }
+        
+        public string PracticeId { get; set; }
+
+        public Patient Patient { get; set; }
+        
+        public string DoctorsId { get; set; }
+
+        public BookedAppointment BookedAppointment { get; set; }
+        
+        public DateTime AppointmentDateTime { get; set; }
+        
+        public bool HasSeenGP { get; set; }
+        
+        public DateTime CreatedDate { get; set; }
+    }
+}
