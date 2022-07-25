fea858b000db895f061db7531d2ddbacc7cf7764
Khanyisani Buthelezi <khanyisani.buthelezi03@gmail.com>
2022-07-25 11:04:22 +0200

    aborting merge conflict

---
+++ SerapisMedicalAPI/Controllers/BillingController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SerapisMedicalAPI.Helpers;
+using SerapisMedicalAPI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SerapisMedicalAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BillingController : ControllerBase
+    {
+        private readonly IBilling _iBilling;
+
+        public BillingController(IBilling iBilling)
+        {
+            _iBilling = iBilling;
+        }
+
+        // GET: api/<BillingController>
+        [HttpGet]
+        public string Get()
+        {
+            return SignatureGenerator.GenerateApiSignature();
+        }
+
+        // GET api/<BillingController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<BillingController>
+        [HttpPost]
+        public string PostNewClientOnBoard([FromBody] string value)
+        {
+            //_iBilling.OnBoardNewClient();
+
+            return value;
+        }
+
+        // PUT api/<BillingController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<BillingController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ SerapisMedicalAPI/Model/ContactDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SerapisMedicalAPI.Model
+{
+    public class ContactDTO
+    {
+        /*public string Email { get; set; }
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public int Age { get; set; }*/
+
+        public IList<Contact> Contacts { get; set; }
+    }
+
+}
